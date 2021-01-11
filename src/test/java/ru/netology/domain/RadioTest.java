c15707bd79289a11f922f2dbbe6130aff890e796
AnastasiaKuznetzova <aleshka1923@gmail.com>
2021-01-11 18:12:59 +0300

    pom

--- src/test/java/ru/netology/domain/RadioTest.java
+++ src/test/java/ru/netology/domain/RadioTest.java
@@ -18,7 +18,18 @@ class RadioTest {
         assertEquals(expected, radio.getCurrentSoundVolume());
 
     }
+    @Test
+    void shouldincreaseSound2() {
+        Radio radio=new Radio();
+        radio.setCurrentSoundVolume(11);
+        radio.setMaxSoundVolume(10);
+        radio.setMinSoundVolume(0);
+        radio.increaseSound();
+        int expected = 10;
 
+        assertEquals(expected, radio.getCurrentSoundVolume());
+
+    }
     @Test
     void shoulddecreaseSound() {
         Radio radio=new Radio();
@@ -31,7 +42,18 @@ class RadioTest {
         assertEquals(expected, radio.getCurrentSoundVolume());
 
     }
+    @Test
+    void shoulddecreaseSound2() {
+        Radio radio=new Radio();
+        radio.setCurrentSoundVolume(-1);
+        radio.setMaxSoundVolume(10);
+        radio.setMinSoundVolume(0);
+        radio.decreaseSound();
+        int expected = 0;
 
+        assertEquals(expected, radio.getCurrentSoundVolume());
+
+    }
     @Test
     void shouldprevRadioStation() {
         Radio radio = new Radio();
@@ -43,7 +65,17 @@ class RadioTest {
 
         assertEquals(expected, radio.getCurrentRadioStation());
     }
+    @Test
+    void shouldprevRadioStation2() {
+        Radio radio = new Radio();
+        radio.setMaxRadioStation(9);
+        radio.setMinRadioStation(0);
+        radio.setCurrentRadioStation(-1);
+        radio.prevRadioStation();
+        int expected = 9;
 
+        assertEquals(expected, radio.getCurrentRadioStation());
+    }
     @Test
     void shouldnnextRadioStation () {
         Radio radio = new Radio();
@@ -52,6 +84,17 @@ class RadioTest {
         radio.setCurrentRadioStation(7);
         radio.nextRadioStation();
         int expected = 8;
+
+        assertEquals(expected, radio.getCurrentRadioStation());
+    }
+    @Test
+    void shouldnnextRadioStation2 () {
+        Radio radio = new Radio();
+        radio.setMaxRadioStation(9);
+        radio.setMinRadioStation(0);
+        radio.setCurrentRadioStation(10);
+        radio.nextRadioStation();
+        int expected = 0;
 
         assertEquals(expected, radio.getCurrentRadioStation());
     }
